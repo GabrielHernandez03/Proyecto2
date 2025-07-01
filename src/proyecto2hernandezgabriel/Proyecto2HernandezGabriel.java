f9758bf4870e745487ef41bc979422f5015362ef
GabrielHernandez03 <gabriel-hernandez@correo.unimet.edu.ve>
2025-07-01 02:11:55 -0400

    Creacion

---
+++ src/proyecto2hernandezgabriel/Proyecto2HernandezGabriel.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto2hernandezgabriel;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Proyecto2HernandezGabriel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
