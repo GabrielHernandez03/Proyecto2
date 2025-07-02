9e57be27da7f1b4b940572055e16699f6b5cf75f
GabrielHernandez03 <gabriel-hernandez@correo.unimet.edu.ve>
2025-07-01 23:14:39 -0400

    Fragmentos

---
+++ src/proyecto2hernandezgabriel/FragmentoADN.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2hernandezgabriel;
+
+/**
+ *
+ * @author gabriel
+ */
+public class FragmentoADN {
+    String frag;
+    int frec;
+    int[] ubi;
+    
+    public FragmentoADN(String f){
+        this.frag = f;
+        this.frec = 0;
+        this.ubi = new int[100];
+    }
+}
