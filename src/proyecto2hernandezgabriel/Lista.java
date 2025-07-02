3f7bfbe92e7629ce892e32526e84628a3606c278
GabrielHernandez03 <gabriel-hernandez@correo.unimet.edu.ve>
2025-07-02 02:52:43 -0400

    Lista 2

--- src/proyecto2hernandezgabriel/Lista.java
+++ src/proyecto2hernandezgabriel/Lista.java
@@ -9,5 +9,34 @@ package proyecto2hernandezgabriel;
  * @author gabriel
  */
 public class Lista {
-    
+    private Nodo primero;
+
+    public Lista() {
+        this.primero = null;
+    }
+
+    public void añadir(FragmentoADN nuevo) {
+        Nodo nuevoNodo = new Nodo(nuevo);
+        if (primero == null) {
+            primero = nuevoNodo; 
+        } else {
+            Nodo actual = primero;
+            while (actual.siguiente != null) {
+                actual = actual.siguiente;
+            }
+            actual.siguiente = nuevoNodo;
+        }
+    }
+
+    public boolean buscar(FragmentoADN frag) {
+        Nodo actual = primero;
+        while (actual != null) {
+            if (actual.dato.equals(frag)) {
+                return true; 
+            }
+            actual = actual.siguiente;
+        }
+        return false;
+    }
 }
+
