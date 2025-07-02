751d4b42a1377f9dd8913a1b102cde1dd2a4c7a9
GabrielHernandez03 <gabriel-hernandez@correo.unimet.edu.ve>
2025-07-02 02:49:22 -0400

    Lista

---
+++ src/proyecto2hernandezgabriel/Lista.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2hernandezgabriel;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Lista {
+    
+}
